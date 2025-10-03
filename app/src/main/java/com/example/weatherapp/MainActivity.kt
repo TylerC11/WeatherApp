ccaae568fd879b8940fb3316f005b123480676be
TylerC11 <w0432196@campus.nscc.ca>
2025-10-03 18:53:37 -0300

    Added Navigation + Top/Bottom bars + removed import

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.weatherapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.weatherapp.R
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
