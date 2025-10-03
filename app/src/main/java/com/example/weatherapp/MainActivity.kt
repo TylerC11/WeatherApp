42cdab0e921a486d3232491043a559d7cf1403e2
TylerC11 <w0432196@campus.nscc.ca>
2025-10-03 18:51:58 -0300

    Added Navigation + Top/Bottom bars

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,55 +2,138 @@ package com.example.weatherapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.example.weatherapp.R
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.weatherapp.ui.screens.CurrentWeatherScreen
 import com.example.weatherapp.ui.screens.DailyForecastScreen
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
+
+
+// Main Activity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            setContent {
-                WeatherAppTheme {
-                    CurrentWeatherScreen()
-                    //DailyForecastScreen()
-                }
-            }
             WeatherAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Weather App",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // Starting point for the app
+                DisplayUI()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun DisplayUI() {
 
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatherAppTheme {
-        Greeting("Android")
+    // Store the selectedIndex in a state flow variable
+    var selectedIndex by remember { mutableIntStateOf(0) }
+
+    Scaffold(
+        topBar = {
+            // Display a top app bar with a title
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+                title = {
+                    Text("Halifax, Nova Scotia")
+                }
+            )
+        },
+        bottomBar = {
+            // Display a navigation bar
+            NavigationBar (
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary
+            )
+            {
+                // Navigation Bar Items go here...
+
+                // Home screen
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_action_current),
+                            contentDescription = "CurrentWeather"
+                        )
+                    },
+                    label = {
+                        Text("Current Weather")
+                    },
+                    selected = selectedIndex == 0,
+                    onClick = {
+                        selectedIndex = 0
+                        navController.navigate(route = "CurrentWeather")
+                    }
+                )
+
+                // Hello screen
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_action_daily),
+                            contentDescription = "DailyForecast"
+                        )
+                    },
+                    label = {
+                        Text("Daily Forecast")
+                    },
+                    selected = selectedIndex == 1,
+                    onClick = {
+                        selectedIndex = 1
+                        navController.navigate(route = "DailyForecast")
+                    }
+                )
+            }
+        }
+    ) { innerPadding ->
+        // Display screens inside a NavHost()
+
+        NavHost(
+            navController = navController,
+            startDestination = "CurrentWeather",
+            modifier = Modifier.padding(innerPadding)
+        )
+        {
+            // Render Home screen
+            composable(route = "CurrentWeather") {
+                CurrentWeatherScreen()
+                //Hello()
+            }
+
+            // Render Hello screen
+            composable(route = "DailyForecast") {
+                DailyForecastScreen()
+            }
+        }
+
     }
+
 }
--- app/src/main/java/com/example/weatherapp/ui/screens/CurrentWeather.kt
+++ app/src/main/java/com/example/weatherapp/ui/screens/CurrentWeather.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import com.example.weatherapp.R
 
 @Composable
@@ -33,8 +32,5 @@ fun CurrentWeatherScreen() {
                 Text("Wind: NE 15 km/h")
             }
         }
-        }
     }
-
-
-
+}
